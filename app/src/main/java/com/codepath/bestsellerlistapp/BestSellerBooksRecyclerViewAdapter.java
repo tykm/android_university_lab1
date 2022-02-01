c444e7193d8a9e49dc879bee0ecbb0b859545514
tykm <tylerkim01@gmail.com>
2022-02-01 17:02:33 -0500

    Having troubles displaying rank but description and link work fine

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -3,6 +3,8 @@ package com.codepath.bestsellerlistapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,8 +37,13 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
     @Override
     public void onBindViewHolder(final BookViewHolder holder, int position) {
         holder.mItem = books.get(position);
+        //holder.mBookRanking.setText(books.get(position).rank);
+        //holder.mBookImage.setImageURI(books.get(position).bookImageUrl);
         holder.mBookTitle.setText(books.get(position).title);
         holder.mBookAuthor.setText(books.get(position).author);
+        holder.mBookDescription.setText(books.get(position).description);
+        holder.mBookBuyButton.setText(books.get(position).amazonUrl);
+
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,15 +64,23 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
 
     public class BookViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
+        public final TextView mBookRanking;
+        public final ImageView mBookImage;
         public final TextView mBookTitle;
         public final TextView mBookAuthor;
+        public final TextView mBookDescription;
+        public final Button mBookBuyButton;
         public BestSellerBook mItem;
 
         public BookViewHolder(View view) {
             super(view);
             mView = view;
+            mBookRanking = (TextView) view.findViewById(R.id.ranking);
+            mBookImage = (ImageView) view.findViewById(R.id.book_image);
             mBookTitle = (TextView) view.findViewById(R.id.book_title);
             mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+            mBookDescription = (TextView) view.findViewById(R.id.book_description);
+            mBookBuyButton = (Button) view.findViewById(R.id.buy_button);
         }
 
         @Override
